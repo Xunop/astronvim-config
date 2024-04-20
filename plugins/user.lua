46b9f87b39d0087a034c822d4525b23a566f7f48
xun <xun794@gmail.com>
2024-04-20 11:36:41 +0800

    Add firenvim plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -39,5 +39,12 @@ return {
   {
     "mfussenegger/nvim-jdtls",
     event = "User AstroFile",
+  },
+  {
+    'glacambre/firenvim',
+    lazy = not vim.g.started_by_firenvim,
+    build = function()
+        vim.fn["firenvim#install"](0)
+    end
   }
 }
